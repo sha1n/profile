10f818c3d0ea7df0b8754c6bbcd2e1b1c909e128
sha1n <sha1n@users.noreply.github.com>
2021-09-26 00:06:55 +0300

    now .zshrc is created if absent

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 autoload -U colors && colors
 local script_dir=${0:a:h}
-local dotzsh="$HOME/.zshrc"
+local dotzshrc="$HOME/.zshrc"
 local dotfiles_dir="$script_dir/dotfiles"
 
 function error() {
@@ -14,14 +14,15 @@ function warn() {
 }
 
 function validate() {
-  print "Observing $dotzsh..."
+  print "Observing $dotzshrc..."
   # We are not going to create .zshrc. If it doesn't exist something is probably off
-  if [[ ! -f "$dotzsh" ]]; then
-    error "the file '$dotzsh' does not exist"
+  if [[ ! -f "$dotzshrc" ]]; then
+    warn "the file '$dotzshrc' does not exist. An empty one will be created."
+    touch "$dotzshrc"
     return 1
   fi
 
-  local existing_source=$(grep -e '^source .*/\.include' "$dotzsh")
+  local existing_source=$(grep -e '^source .*/\.include' "$dotzshrc")
   if [[ ! -z "$existing_source" ]]; then
     error "the following 'source' command is already in your .zshrc profile: $existing_source"
     return 1
@@ -30,10 +31,10 @@ function validate() {
 
 function install() {
   print "Installing profile..."
-  echo "source '$script_dir/.include'" >>"$dotzsh"
+  echo "source '$script_dir/.include'" >>"$dotzshrc"
   if [[ "$?" == "0" ]]; then
     echo "Installed successfully!"
-    echo "To verify installation start new session or source $dotzsh"
+    echo "To verify installation start new session or source $dotzshrc"
   fi
 }
 
