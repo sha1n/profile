13d3513d83e91730a8e7d9844c474976615ef746
Shai Nagar <sha1n@users.noreply.github.com>
2021-09-28 16:33:35 +0300

    bazel setup (#6)
    * changed log style
    
    * added basic bazel setup

--- scripts/lib.zsh
+++ scripts/lib.zsh
@@ -2,21 +2,21 @@
 autoload -U colors && colors
 
 function __profile_log_error() {
-  __profile_log_log "red" "error" $1
+  __profile_log_log "red" "ERROR" $1
 }
 
 function __profile_log_warn() {
-  __profile_log_log "yellow" "warn" $1
+  __profile_log_log "yellow" "WARNING" $1
 }
 
 function __profile_log_info() {
-  __profile_log_log "cyan" "info" $1
+  __profile_log_log "cyan" "INFO" $1
 }
 
 function __profile_log_success() {
-  __profile_log_log "green" "success" $1
+  __profile_log_log "green" "SUCCESS" $1
 }
 
 function __profile_log_log() {
-  printf "$fg[$1]%7s:$reset_color %s\n" "$2" "$3"
+  printf "$fg[$1]%s:$reset_color %s\n" "$2" "$3"
 }
